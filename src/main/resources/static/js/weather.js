4288c73932f111307deb17155539001fcacac9df
Panchev <neno.panchev@gmail.com>
2022-11-23 17:49:49 +0200

    fixing weather fetching to be async function

--- src/main/resources/static/js/weather.js
+++ src/main/resources/static/js/weather.js
@@ -1,10 +1,13 @@
 const boxImgA = document.getElementById('box-a-img');
-const boxImgB = document.getElementById('box-b-img');
 const boxTempA = document.getElementById('box-a-temp');
-const boxTempB = document.getElementById('box-b-temp');
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Plovdiv&appid=e80b8bd999a5a20029a930973b3e8a4b")
-    .then(data => data.json())
+async function fetchWeatherJSON() {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Plovdiv&appid=e80b8bd999a5a20029a930973b3e8a4b");
+    const weather = await response.json();
+    return weather;
+}
+
+fetchWeatherJSON()
     .then(info => {
         //Formula Kelvin to Celsius 299K − 273.15 = 25.85°C
         boxTempA.innerText = Math.round(info.main.temp - 273.15);
